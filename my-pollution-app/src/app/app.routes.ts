b7063b8eb13bf62ea434c6ce954838fe8d0c4df2
Mathilde <maguillemain@unistra.fr>
2025-11-11 22:45:07 +0100

    Ajout utilisateurs

--- my-pollution-app/src/app/app.routes.ts
+++ my-pollution-app/src/app/app.routes.ts
@@ -2,15 +2,14 @@ import { Routes } from '@angular/router';
 import { PollutionListComponent } from './pollution-list/pollution-list.component';
 import { PollutionFormComponent } from './pollution-form/pollution-form.component';
 import { PollutionRecapComponent } from './pollution-recap/pollution-recap.component';
-//import { UserListComponent } from './user-list/user-list.component';
-//import { UserFormComponent } from './user-form/user-form.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'pollutions', pathMatch: 'full' }, // redirige vers la liste
+  { path: '', redirectTo: 'pollutions', pathMatch: 'full' },
   { path: 'pollutions', component: PollutionListComponent },
   { path: 'add-pollution', component: PollutionFormComponent },
   { path: 'pollution/:id', component: PollutionRecapComponent },
-    // Routes pour les utilisateurs 👇
-  //{ path: 'users', component: UserListComponent },
-  //{ path: 'add-user', component: UserFormComponent },
+  { path: 'utilisateur', component: UserListComponent },
+  { path: 'add-user', component: UserFormComponent },
 ];
